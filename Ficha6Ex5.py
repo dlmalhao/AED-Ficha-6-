58235492b24c60fb3bc1777f1cfb04988732ed1c
dlmalhao <dlmalhao@gmail.com>
2020-11-16 16:52:24 +0000

    Ex 5 Versão final

--- Ficha6Ex5.py
+++ Ficha6Ex5.py
@@ -1,21 +1,16 @@
 #Ler a faturação mensal de uma empresa ao longo de 12 meses (janeiro a dezembro) e indicar o mes de maior e menor faturação e a media mensal de faturação
 
-def maior_faturacao(lista_meses, lista_valores):
-    lista_maior_mes = []
-    lista_maior_valor = []
-    for i in range (len(lista_valores)):
-        lista_maior_valor.append(max(lista_valores))
-        lista_maior_mes.append(lista_meses[i])
-    return lista_maior_mes, lista_maior_valor
+def maior_faturacao(lista_valores):
+    maior = max(lista_valores)
+    pos = lista_valores.index(maior)
+    mes = lista_meses[pos]
+    return mes
 
-def menor_faturacao(lista_meses, lista_valores):
-    lista_menor_mes = []
-    lista_menor_valor = []
-    for i in range (len(lista_valores)):
-        lista_menor_valor.append(min(lista_valores))
-        lista_menor_mes.append(lista_meses[i])
-    return lista_menor_mes, lista_menor_valor
-
+def menor_faturacao(lista_valores):
+    menor = min(lista_valores)
+    pos = lista_valores.index(menor)
+    mes = lista_meses[pos]
+    return mes
 
 lista_meses = []
 lista_valores = []
@@ -26,7 +21,7 @@ for i in range(12):
         try:
             valor = int(input("\n\n\t\t Insira o valor: "))
             if valor <0 :
-                raise ValueError
+                raise ValueError()
             lista_meses.append(meses)
             lista_valores.append(valor)
         except ValueError:
@@ -35,20 +30,10 @@ for i in range(12):
             valido = True   
 
 media = sum(lista_valores) / 12
-
-lista_maior_mes = maior_faturacao(lista_meses, lista_valores)
-lista_menor_mes = menor_faturacao(lista_meses, lista_valores)
-print("Mês de maior faturação: {0}".format(lista_maior_mes))
-
-print("Mês de menor faturação: {0}".format(lista_menor_mes))
-
 print("Média: {0}".format(media))
 
+resultado_maior = maior_faturacao(lista_valores)
+resultado_menor = menor_faturacao(lista_valores)
 
-    
-    
-
-
-    
-
-    
+print("Mês de maior faturação: {0}".format(resultado_maior))
+print("Mês de menor faturação: {0}".format(resultado_menor))
