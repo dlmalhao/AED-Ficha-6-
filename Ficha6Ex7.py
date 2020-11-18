34bead729b33c3e500c1144c1e6e76f4f0feca15
dlmalhao <dlmalhao@gmail.com>
2020-11-18 00:10:11 +0000

    Exercicio 7

--- Ficha6Ex7.py
+++ Ficha6Ex7.py
@@ -1,25 +1,30 @@
 
 
+import os
+
 def ordenar(lista_valor):
-    lista_valor.sort()
-    lista_valor.reverse()
-    return lista_valor
+    lista_copy = lista_valor.copy()
+    lista_copy.sort()              #Duvidas em como ordenar os meses na funçáo
+    lista_copy.reverse()
+    for i in range(12):
+        pos = lista_valor.index(lista_copy[i])
+        print(mes[pos],"\t" ,lista_copy[i])
+        lista_valor[pos] = -1
 
 
-
+mes = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
 lista_valor = []
 for i in range(12):
     valido = False
     while not valido:
         try:
-            numero = int(input("\n\n\t\tValor: "))
-            if numero < 0:
+            valor = int(input("\n\n\t\tValor no mes de {0}: ".format(mes[i])))
+            if valor < 0:
                 raise ValueError()
-            lista_valor.append(numero)
+            lista_valor.append(valor)
         except ValueError:
             print("\n\n\t\tFormato incorreto, por favor tente novamente: ")
         else:
             valido = True    
 
-lista_ordenada = ordenar(lista_valor)
-print("\n\n\t\t{0}".format(lista_ordenada))
+ordenar(lista_valor)
